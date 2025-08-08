a5812d53acbe78d80b0bf4f60f7ef7a269ee637d
theprogmatheus <theprog.matheus@gmail.com>
2025-08-08 18:31:37 -0300

    Create SqlQueryLoader

---
+++ src/main/java/com/github/theprogmatheus/devroom/treasurehunt/database/SqlQueryLoader.java
@@ -0,0 +1,52 @@
+package com.github.theprogmatheus.devroom.treasurehunt.database;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SqlQueryLoader {
+
+    private final String tablePrefix;
+    private final Map<String, String> cachedQueries;
+
+    public SqlQueryLoader(String tablePrefix) {
+        this.tablePrefix = tablePrefix;
+        this.cachedQueries = new HashMap<>();
+    }
+
+
+    public String getQuery(String queryPath) {
+        return this.cachedQueries
+                .computeIfAbsent(queryPath, key -> loadQuery(queryPath));
+    }
+
+    private String loadQuery(String queryPath) {
+        InputStream resource = getClass().getResourceAsStream("/sql/%s.sql".formatted(queryPath));
+        if (resource == null)
+            throw new IllegalArgumentException("The query '%s' not found.".formatted(queryPath));
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Scanner scanner = new Scanner(new InputStreamReader(resource, StandardCharsets.UTF_8))) {
+            while (scanner.hasNextLine()) {
+                stringBuilder.append(scanner.nextLine()).append(System.lineSeparator());
+            }
+        }
+
+        return stringBuilder.toString()
+                .replace("%table_prefix%", this.tablePrefix)
+                .trim();
+    }
+
+
+    public String getTablePrefix() {
+        return tablePrefix;
+    }
+
+    public Map<String, String> getCachedQueries() {
+        return cachedQueries;
+    }
+}
